7bf8a706639521406d2f5f3938ad5f3723184356
Suzanna Lucille Moran <suzannas@comp211-2sp23.cs.unc.edu>
2023-04-27 12:29:24 -0400

    fixed check, recover, exit--all working

--- tetrashell.c
+++ tetrashell.c
@@ -7,10 +7,10 @@
 
 
 int main(int argc, char* argv[]){
-	
 	char *command = malloc(sizeof(char) * 100);
 	char *program = malloc(sizeof(char) * 50);
 	char **my_args = (char**) malloc(sizeof(char*) * 5);
+	char *token = malloc(sizeof(char) * 50);
 	int i = 1;
 
 	for (int i = 0; i < 5; i++){
@@ -18,19 +18,22 @@ int main(int argc, char* argv[]){
 	}
 
 	printf("starting\n");
-	while (strcmp(program, "exit") != 0){	
+	while (strcmp(token, "exit") != 0){	
 		printf("tetrashell> ");
 		fgets(command, 100, stdin);
+		command[strlen(command) - 1] = '\0';
 		program = strtok(command, " ");
+		strcpy(token, program);	
 		strcpy(my_args[0], program);
 		while (program = strtok(NULL, " ")){
-		strcpy(my_args[i], program);
-		++i;
+			strcpy(my_args[i], program);
+			i++;
 		}
-		strcpy(my_args[i+1], "\0");
+		my_args[i] = NULL;	
+		//strcpy(my_args[i+1], "\0");
 		//my_args[1] = strtok(command, " ");
-	
-	//scanf("%s", program);
+
+		//scanf("%s", program);
 		if(strcmp(my_args[0], "exit") == 0){
 			exit(1);		
 		}
@@ -39,23 +42,35 @@ int main(int argc, char* argv[]){
 
 		pid_t pid = fork();
 		if (pid){
-		printf("in parent\n");
-		int res;
-		wait(&res);
+			printf("in parent\n");
+			int res;
+			wait(&res);
 		} else {
 			printf("in child\n");	
 			if (strcmp(my_args[0], "recover") == 0){
-        	                printf("recovering\n");
+				printf("saw recover\n");
+				//my_args[0] = strcat("./", my_args[0]);                        
+				//printf("set first arg to ./recover\n");
 			}
 			else if (strcmp(my_args[0], "check") == 0){
 				printf("checking\n");
 			}	
-
+			for (int j = 0; j < i; j++){
+				printf("%s\n", my_args[j]);
+			}
 			printf("about to execv\n");
 			execv(my_args[0], my_args);
-			printf("returning to parent.\n");
-			free(program);
-			free(command);
+			//printf("you're not supposed to be in here\n");
+			/*
+			   free(command);
+			   printf("freed command\n");
+			   printf("program name: %s\n", my_args[0]);
+			   program = my_args[0];
+			   for (int i = 0; i < 5; i++){
+			   free(my_args[i]);
+			   }
+			   printf("freed args list\n");
+			 */
 			return 0;}}
 
 	return 0;}
