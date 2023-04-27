041440e8b379dba21bb0ce58ad5480542d833463
Suzanna Lucille Moran <suzannas@comp211-2sp23.cs.unc.edu>
2023-04-27 00:02:06 -0400

    first

--- tetrashell.c
+++ tetrashell.c
@@ -12,45 +12,44 @@ char *program;
 char *my_args[4];
 int i = 1;
 
-while (strcmp(program, "exit") != 0){
-        printf("tetrashell> ");
-        fgets(command, 50, stdin);
-        program = strtok(command, " ");
-        my_args[0] = program;
-        while (program = strtok(NULL, " ")){
-        strcpy(my_args[i], program);
-        ++i;
-        }
-        my_args[i+1] = NULL;
-        //my_args[1] = strtok(command, " ");
-
+while (strcmp(program, "exit") != 0){	
+	printf("tetrashell> ");
+	fgets(command, 100, stdin);
+	program = strtok(command, " ");
+	my_args[0] = program;
+/*	while (program = strtok(NULL, " ")){
+	strcpy(my_args[i], program);
+	++i;*/
+//	}
+	my_args[i+1] = NULL;
+	//my_args[1] = strtok(command, " ");
+	
 //scanf("%s", program);
-        if(strcmp(my_args[0], "exit") == 0){
-                exit(1);
-        }
-        printf("line 20\n");
+	if(strcmp(my_args[0], "exit") == 0){
+		exit(1);		
+	}
+	printf("line 20\n");
 
 
-        pid_t pid = fork();
-        if (pid){
-        printf("in parent\n");
-        int res;
-        wait(&res);
-        } else {
-                printf("in child\n");
-                if (strcmp(my_args[0], "recover") == 0){
+	pid_t pid = fork();
+	if (pid){
+	printf("in parent\n");
+	int res;
+	wait(&res);
+	} else {
+		printf("in child\n");	
+		if (strcmp(my_args[0], "recover") == 0){
                         printf("recovering\n");
-                }
-                else if (strcmp(my_args[0], "check ") == 0){
-                        printf("checking\n");
-                }
+		}
+		else if (strcmp(my_args[0], "check ") == 0){
+			printf("checking\n");
+		}	
 
-                printf("about to execv\n");
-                execv(my_args[0], my_args);
-                printf("returning to parent.\n");
-
-                return 0;}}
-//free(program);}
+		printf("about to execv\n");
+		execv(my_args[0], my_args);
+		printf("returning to parent.\n");
+		free(program);
+		free(command);
+		return 0;}}
 
 return 0;}
-
