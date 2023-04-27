eb69eeeb027d3129bc797b97227d0791269b6f87
narwood narwood <narwood@comp211-2sp23.cs.unc.edu>
2023-04-26 23:48:11 -0400

    first commit

---
+++ tetrashell.c
@@ -0,0 +1,56 @@
+#include <stdio.h>
+#include <string.h>
+#include <sys/types.h>
+#include <sys/wait.h>
+#include <unistd.h>
+#include <stdlib.h>
+
+
+int main(int argc, char* argv[]){
+char *command = malloc(sizeof(char) * 100);
+char *program;
+char *my_args[4];
+int i = 1;
+
+while (strcmp(program, "exit") != 0){
+        printf("tetrashell> ");
+        fgets(command, 50, stdin);
+        program = strtok(command, " ");
+        my_args[0] = program;
+        while (program = strtok(NULL, " ")){
+        strcpy(my_args[i], program);
+        ++i;
+        }
+        my_args[i+1] = NULL;
+        //my_args[1] = strtok(command, " ");
+
+//scanf("%s", program);
+        if(strcmp(my_args[0], "exit") == 0){
+                exit(1);
+        }
+        printf("line 20\n");
+
+
+        pid_t pid = fork();
+        if (pid){
+        printf("in parent\n");
+        int res;
+        wait(&res);
+        } else {
+                printf("in child\n");
+                if (strcmp(my_args[0], "recover") == 0){
+                        printf("recovering\n");
+                }
+                else if (strcmp(my_args[0], "check ") == 0){
+                        printf("checking\n");
+                }
+
+                printf("about to execv\n");
+                execv(my_args[0], my_args);
+                printf("returning to parent.\n");
+
+                return 0;}}
+//free(program);}
+
+return 0;}
+
